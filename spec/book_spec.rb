2d44f7d402c3ed1067effcc67c40e4ac3316f2e6
LTurns <lizzieturney@gmail.com>
2020-04-12 11:36:23 +0100

    ensuring the correct parameters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,16 +1,26 @@
-require './spec_helper'
+require 'spec_helper'
 
 #all tests are passed into a block. Here we are using Book as a class.
 describe Book do
 
  #Before means this section will be run before each test.
- Before: each do
+ before :each do
    #create an instance variable inside the class (so it is accessible everywhere)
    @book = Book.new "Title", "Author", :category
-   
  end
 
 #tests begin with "it" and have a do and end.
- it "" do
+ describe "#new" do
+ it "returns a new book object" do
+   @book.should( be_an_instance_of ( Book ))
+ end
+
+ it "takes three parameters and returns a book object" do
+   lambda { Book.new "Title", "Author"}.should raise_exception ArgumentError
+
+ end
+
  end
 end
+
+#'Book#new'is a convention which means the same as Classname#methodname
